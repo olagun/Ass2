445385b0aefe660f3e25dc2db9760ee69f48a9d0
Sam Olaogun <sam.olaogun@gmail.com>
2020-04-24 11:47:10 -0400

    Fixed hashing issue

--- src/commands/add.c
+++ src/commands/add.c
@@ -36,8 +36,18 @@ void add_client(char* project_name, char* file_path) {
   // https://rosettacode.org/wiki/SHA-256#C
   char* file_bytes = filelist->file_bytes;
   int file_size = filelist->file_size;
-  filelist->file_hash =
-      (char*)SHA256((const unsigned char*)file_bytes, file_size, 0);
+
+  unsigned char* raw_hash =
+      SHA256((unsigned const char*)file_bytes, file_size, 0);
+  char* hex_hash = calloc(SHA256_DIGEST_LENGTH * 2 + 1, sizeof(char));
+
+  int i = 0;
+  for (; i < SHA256_DIGEST_LENGTH; i++) {
+    // Print two hex chars (0x00) for every byte
+    sprintf(hex_hash + 2 * i, "%02x", raw_hash[i]);
+  }
+
+  filelist->file_hash = hex_hash;
 
   // Read in project's `.Manifest`
   Manifest* manifest = manifest_read(project_name);
